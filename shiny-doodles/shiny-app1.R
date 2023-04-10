0531541fa8abd31fd0045df4e12e01fb80538a5d
Dave Braze <davebraze@gmail.com>
2023-04-10 11:41:39 -0400

    2 plots, 1 page

--- shiny-doodles/shiny-app1.R
+++ shiny-doodles/shiny-app1.R
@@ -8,93 +8,140 @@ library(MetBrewer)
 library(plotly)
 
 ## Read data file
-data <- read.csv(here::here("00-data", "01-xls-processed-data", "D2.csv"))
+data <-
+    read.csv(here::here("00-data", "01-xls-processed-data", "D2.csv")) %>%
+    filter(year >= 2003)
+
 ylimits <- range(data$avg.scale.score, na.rm = TRUE)
 
-## Define UI
+#### UI
 ui <- fluidPage(
-    ## Define title
-    titlePanel("2019 NAEP Reading Scores for All 50 States"),
+    titlePanel("4th Grade NAEP Reading Score Gap by State"),
 
-    ## Define sidebar layout
     sidebarLayout(
-        ## Define sidebar panel
         sidebarPanel(
             selectInput("selected_year", "Choose a year:",
                         choices = unique(data$year),
                         selected = max(data$year))
           , actionButton("quit", "Quit")
+          , width =2
         ),
-        ## Define main panel
         mainPanel(
-            ## Define plot output
-            plotOutput("barbell")
+            ## plots to render in main panel
+            plotlyOutput("naep_by_state")
+            , plotlyOutput("ct_naep_by_year")
         )
     )
 )
 
-## Define server
-server <- function(input, output) {
+##### server
+    server <- function(input, output) {
 
-    observeEvent(input$quit, {
-        stopApp()
-    })
+        observeEvent(input$quit, {
+            stopApp()
+        })
 
-    data_filtered <- reactive({
+        data_all_states <- reactive({
 
-        data %>%
-            filter(race.ethnicity %in% c("Black", "White"),
-                   year==input$selected_year) %>%
-            group_by(jurisdiction) %>%
-            mutate(wscore = max(avg.scale.score),
-                   bscore = min(avg.scale.score),
-                   wbdiff = diff(avg.scale.score)) %>%
-            ungroup() %>%
-            mutate(jurisdiction = fct_reorder(jurisdiction, wbdiff),
-                   rank.gap = ceiling(rank(wbdiff)/2)) %>%
-            mutate(picks = case_when(jurisdiction == "Connecticut" ~ "B",
-                                     jurisdiction == "National Avg." ~ "A",
-                                     !jurisdiction %in% c("Connecticut", "National Avg.") ~ "C")) %>%
-            group_by(jurisdiction)
+            data %>%
+                filter(race.ethnicity %in% c("Black", "White"),
+                       year==input$selected_year) %>%
+                group_by(jurisdiction) %>%
+                mutate(wscore = max(avg.scale.score),
+                       bscore = min(avg.scale.score),
+                       wbdiff = diff(avg.scale.score)) %>%
+                ungroup() %>%
+                mutate(jurisdiction = fct_reorder(jurisdiction, wbdiff),
+                       rank.gap = ceiling(rank(wbdiff)/2)) %>%
+                mutate(picks = case_when(jurisdiction == "Connecticut" ~ "B",
+                                         jurisdiction == "National Avg." ~ "A",
+                                         !jurisdiction %in% c("Connecticut", "National Avg.") ~ "C")) %>%
+                group_by(jurisdiction)
 
-    })
+        })
 
-    ## Define plot
-    output$barbell <- renderPlot({
+        ## Define plot using ggplot > ggplotly
+        output$naep_by_state <- renderPlotly({
 
-        p1 <-
-            ggplot(data_filtered(), aes(x=jurisdiction, y=avg.scale.score, alpha=picks,
-                                        group=jurisdiction, color=race.ethnicity,
-                                        text=paste0(as.character(jurisdiction),
-                                                    "\n   Black: ", formatC(bscore, format="f", digits=2),
-                                                    "\n   White: ", formatC(wscore, format="f", digits=2),
-                                                    "\n   Gap: ", formatC(wbdiff, format="f", digits=2),
-                                                    "\n   Rank: ", formatC(rank.gap, format="d")
-                                                    ))) +
-            geom_line(color="black") +
-            geom_point(size=2) +
-            scale_color_manual(values=rev(met.brewer("Cassatt2", 2))) +
-            scale_alpha_manual(values=c(A = 1, B = 1, C = 1/4)) +
-            guides(color="none", alpha="none") +
-            scale_y_continuous(limits = ylimits) +
-            theme_minimal() +
-            theme(axis.text.x = element_text(angle=90, hjust=1, vjust=.4)) +
-            labs(title=paste0("Black/White Gap in NAEP 4th Grade Reading Scores by State -- ", input$selected_year)) +
-            ylab ("Average 4th Grade Reading Scores") +
-            xlab ("")
+            p1 <-
+                ggplot(data_all_states(), aes(x=jurisdiction, y=avg.scale.score, alpha=picks,
+                                            group=jurisdiction, color=race.ethnicity,
+                                            text=paste0(as.character(jurisdiction),
+                                                        "\n   Black: ", formatC(bscore, format="f", digits=2),
+                                                        "\n   White: ", formatC(wscore, format="f", digits=2),
+                                                        "\n   Gap: ", formatC(wbdiff, format="f", digits=2),
+                                                        "\n   Rank: ", formatC(rank.gap, format="d")
+                                                        ))) +
+                geom_line(color="black") +
+                geom_point(size=2) +
+                scale_color_manual(values=rev(met.brewer("Cassatt2", 2))) +
+                scale_alpha_manual(values=c(A = 1, B = 1, C = 1/4)) +
+                guides(color="none", alpha="none") +
+                scale_y_continuous(limits = ylimits) +
+                theme_minimal() +
+                theme(axis.text.x = element_text(angle=90, hjust=1, vjust=.4)) +
+                labs(title=paste0("Black/White Gap in NAEP 4th Grade Reading Scores by State -- ", input$selected_year)) +
+                ylab ("Average 4th Grade Reading Scores") +
+                xlab ("")
 
-        p1
+            ggplotly(p1, tooltip = c("text"), layerData=1) %>%
+                hide_legend() %>%
+                config(modeBarButtonsToRemove = c('zoomIn2d', 'zoomOut2d', 'pan2d',
+                                                  'select2d', 'lasso2d',
+                                                  'hoverCompare', 'hoverClosestCartesian')) %>%
+                layout(xaxis = list(fixedrange = TRUE),
+                       yaxis = list(fixedrange = TRUE))
 
-        ## ggplotly(p1, tooltip = c("text"), layerData=1) %>%
-        ##     hide_legend() %>%
-        ##     config(modeBarButtonsToRemove = c('zoomIn2d', 'zoomOut2d', 'pan2d',
-        ##                                       'select2d', 'lasso2d',
-        ##                                       'hoverCompare', 'hoverClosestCartesian')) %>%
-        ##     layout(xaxis = list(fixedrange = TRUE),
-        ##            yaxis = list(fixedrange = TRUE))
+        })
 
-    })
-}
+        output$ct_naep_by_year <- renderPlotly({
+
+            D2.ct <-
+                data %>%
+                filter(race.ethnicity %in% c("Black", "White"),
+                       jurisdiction == "Connecticut",
+                       year >= 2003) %>%
+                group_by(year) %>%
+                mutate(wscore = max(avg.scale.score),
+                       bscore = min(avg.scale.score),
+                       wbdiff = diff(avg.scale.score)) %>%
+                ungroup() %>%
+                mutate(picks = case_when(jurisdiction == "Connecticut" ~ "B",
+                                         jurisdiction == "National Avg." ~ "A",
+                                         !jurisdiction %in% c("Connecticut", "National Avg.") ~ "C"))
+
+                p3 <-
+                    D2.ct %>%
+                    ggplot(aes(x=year, y=avg.scale.score, alpha=picks,
+                               group=year, color=race.ethnicity,
+                               text=paste0(as.character(jurisdiction),
+                                           "\n  Black: ", formatC(bscore, format="f", digits=2),
+                                           "\n  White: ", formatC(wscore, format="f", digits=2),
+                                           "\n  Gap: ", formatC(wbdiff, format="f", digits=2)
+                                           ))) +
+                    geom_line(color="black") +
+                    geom_point(size=2) +
+                    scale_color_manual(values=rev(met.brewer("Cassatt2", 2))) +
+                    scale_alpha_manual(values=c(A = 1, B = 1, C = 1/4)) +
+                    scale_x_continuous(breaks=seq(2003, 2019, 2), name="") +
+                    theme_minimal() +
+                    ## theme(axis.text.x = element_text(angle=90, hjust=1, vjust=.4)) +
+                    labs(title="Black/White Gap in CT 4th Grade Reading Scores by Year") +
+                    ylab ("Average 4th Grade Reading Scores") +
+                    xlab ("")
+
+                    ggplotly(p3, tooltip="text") %>%
+                        hide_legend() %>%
+                        config(modeBarButtonsToRemove = c('zoomIn2d', 'zoomOut2d', 'pan2d',
+                                                          'select2d', 'lasso2d',
+                                                          'hoverCompare', 'hoverClosestCartesian')) %>%
+                        layout(xaxis = list(fixedrange = TRUE),
+                               yaxis = list(fixedrange = TRUE))
+
+
+        })
+
+    }
 
 # Run the app
 shinyApp(ui, server)
